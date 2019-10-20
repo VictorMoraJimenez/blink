cc86d56b85fe48671de359f6c71ce3ad2222954d
VictorMoraJimenez <36267572+VictorMoraJimenez@users.noreply.github.com>
2019-10-20 20:33:03 +0200

    Blink with millis() function

--- Blink.ino
+++ Blink.ino
@@ -22,16 +22,31 @@
   http://www.arduino.cc/en/Tutorial/Blink
 */
 
+//Constants 
+const int ledPin =  LED_BUILTIN;
+const long intervalMs = 2000;
+//Variables
+int ledState = LOW;
+unsigned long prevMs;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(3000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+
+  if(millis()-prevMs >= intervalMs){
+    prevMs = millis();
+
+    if(ledState == LOW ){
+      ledState = HIGH; 
+    }else {
+      ledState = LOW;  
+    }
+    digitalWrite(ledPin, ledState);
+  }
 }
+
